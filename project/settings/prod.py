e08abd46d985ba1422b408fe6981c6d71f9fcc19
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-05 12:49:04 +0200

    Allowing HOSTS

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -5,10 +5,9 @@ from defaults import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-# ALLOWED_HOSTS = [
-#     '.arcsecond.io',
-#     'arcsecond-prod.herokuapp.com'
-# ]
+ALLOWED_HOSTS = [
+    'politweets-prod.herokuapp.com'
+]
 
 SITE_ID = 3  # Local=1, Staging=2, Prod=3
 
