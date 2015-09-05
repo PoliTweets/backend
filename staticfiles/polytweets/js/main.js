a0cc70eb66e4893b058b54e37bac893dbc78814c
angelo <angelo@10.33.122.124>
2015-09-05 16:16:18 +0200

    js

--- staticfiles/polytweets/js/main.js
+++ staticfiles/polytweets/js/main.js
@@ -1,1 +1,9 @@
-
+ $(document).ready(function () {
+    $("input:radio").change(function () {
+        if ($('input:radio[name=type]:checked').val() == $('#party_name_key').value) {
+            alert('OK');
+        } else {
+            alert('NO OK');
+        }
+    });
+});
