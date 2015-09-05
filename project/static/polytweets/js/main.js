9ced6c10df61d2f1ea80efca97c71921d5a67475
angelo <angelo@10.33.122.124>
2015-09-05 16:11:10 +0200

    js

--- project/static/polytweets/js/main.js
+++ project/static/polytweets/js/main.js
@@ -1,1 +1,10 @@
-
+ $(document).ready(function () {
+    $("input:radio").change(function () {
+        if ($('input:radio[name=type]:checked').val() == $('#party_name_key').value) {
+            alert('OK');
+            //$('#select-table > .roomNumber').attr('enabled',false);
+        } else {
+            alert('NO OK');
+        }
+    });
+});
