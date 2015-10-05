5aaa4ada08bc0fae541050dea1129a08c442923f
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-05 10:11:29 +0200

    Better debug message

--- project/static/politweets/js/main.js
+++ project/static/politweets/js/main.js
@@ -1,12 +1,7 @@
- $(document).ready(function () {
+$(document).ready(function () {
     $("input:radio").change(function () {
-        console.log($('input:radio[name=type]:checked').val());
-        console.log($('#party_name_key').val());
-        if ($('input:radio[name=type]:checked').val() == $('#party_name_key').val()) {
-            alert('OK');
-            //$('#select-table > .roomNumber').attr('enabled',false);
-        } else {
-            alert('NO OK');
-        }
+        var answer = $('input:radio:checked').val();
+        var correct_answer = $('#party_name_key').val();
+        alert('You selected '+answer+', the correct answer is '+correct_answer);
     });
 });
