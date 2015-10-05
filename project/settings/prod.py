0ea200e46bce1590a54263c7c1fb9c45ac9eff68
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-05 21:12:44 +0200

    Following advice of error messages...

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -5,8 +5,9 @@ from defaults import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'politweets-prod.herokuapp.com',
-    'lab.rts.ch'
+    u'politweets-prod.herokuapp.com',
+    u'lab.rts.ch',
+    u'10.102.67.64'
 ]
 
 SITE_ID = 3  # Local=1, Staging=2, Prod=3
