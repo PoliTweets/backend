cef1b92dd5f0b980a28692728a0a4913bf4abaea
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-05 14:52:00 +0200

    Fixing parties properties as well as candidates, and improved linking between the two + making the 'results' page work!

--- project/static/politweets/js/main.js
+++ project/static/politweets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $("input:radio").change(function () {
-        var correct_answer = $('#party_name_key').val(); // Correct answer is placed in hidden input at this stage.
+        var correct_answer = $('#party_key').val(); // Correct answer is placed in hidden input at this stage.
         var answer = $('input:radio:checked').val(); // Get answer
-        $('#party_name_key').val(answer); // Replace correct answer with current one in hidden form input.
+        $('#party_key').val(answer); // Replace correct answer with current one in hidden form input.
 
         $('input:radio').attr("disabled",true);
         $('#correct_answer').show();
