91a35e8b10cbb537621f0f7174c57a10e9c147ae
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-05 10:31:35 +0200

    Much imrpoved workflow

--- project/static/politweets/js/main.js
+++ project/static/politweets/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $("input:radio").change(function () {
         var answer = $('input:radio:checked').val();
         var correct_answer = $('#party_name_key').val();
-        alert('You selected '+answer+', the correct answer is '+correct_answer);
+        $('input:radio').attr("disabled",true);
+        $('#correct_answer').show();
+        $('#nextButton').show();
     });
 });
