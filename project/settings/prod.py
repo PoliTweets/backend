0c1f0793e4d5b4940f8fdfc768ca44c398dde2ae
Mounir Krichane <mounir.krichane@rts.ch>
2015-10-05 15:27:48 +0200

    Modified settings for deployment on the MPC

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -6,7 +6,8 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = [
-    'politweets-prod.herokuapp.com'
+    'politweets-prod.herokuapp.com',
+    'lab.rts.ch'
 ]
 
 SITE_ID = 3  # Local=1, Staging=2, Prod=3
